412fd4936c5e2048d04d42bddf95c7e10e75e718
Ronishan-18 <ronisonroni0@gmail.com>
2026-01-05 12:27:02 +0530

    fourthcommit

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,17 @@ function App() {
   const myBooks = [
     { 
       title: "To Kill a Mockingbird", 
-      author: "Harper Lee", 
+      author: "Abiyaan,kapi", 
       description: "A novel set in the American South during the 1930s." 
     },
     { 
       title: "1984", 
-      author: "George Orwell", 
+      author: "dishanth,Thuva", 
       description: "A dystopian novel set in a totalitarian regime." 
     },
     { 
       title: "The Great Gatsby", 
-      author: "F. Scott Fitzgerald", 
+      author: "Sai,saran", 
       description: "A novel depicting the Jazz Age and the American Dream." 
     }
   ];
